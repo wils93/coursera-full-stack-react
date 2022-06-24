61185f0309f7d2cf0effe5611af7892ff33f50c6
Andrew Wilson <andero.wil@gmail.com>
2022-06-25 00:02:47 +0200

    Use Gulp part 2 (Automation of build process)

--- 01. Front-End Web UI Frameworks and Tools Bootstrap 4/01. Weeks/Bootstrap4/conFusion/gulpfile.js
+++ 01. Front-End Web UI Frameworks and Tools Bootstrap 4/01. Weeks/Bootstrap4/conFusion/gulpfile.js
@@ -2,7 +2,15 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    browserSync = require('browser-sync');
+    browserSync = require('browser-sync'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cleanCss = require('gulp-clean-css'),
+    flatmap = require('gulp-flatmap'),
+    htmlmin = require('gulp-htmlmin');
 
 gulp.task('sass', function () {
     return gulp.src('./css/*.scss')
@@ -11,7 +19,7 @@ gulp.task('sass', function () {
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch('./css/*.scss', ['sass']);
+    gulp.watch('./css/*.scss', gulp.series('sass'));
 });
 
 gulp.task('browser-sync', function () {
@@ -30,7 +38,41 @@ gulp.task('browser-sync', function () {
 
 });
 
+// Clean
+gulp.task('clean', function () {
+    return del(['dist']);
+});
+
+gulp.task('copyfonts', function () {
+    return gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+        .pipe(gulp.dest('./dist/fonts'));
+});
+
+// Images
+gulp.task('imagemin', function () {
+    return gulp.src('img/*.{png,jpg,gif}')
+        .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('usemin', function () {
+    return gulp.src('./*.html')
+        .pipe(flatmap(function (stream, file) {
+            return stream
+                .pipe(usemin({
+                    css: [rev()],
+                    html: [function () { return htmlmin({ collapseWhitespace: true }) }],
+                    js: [uglify(), rev()],
+                    inlinejs: [uglify()],
+                    inlinecss: [cleanCss(), 'concat']
+                }))
+        }))
+        .pipe(gulp.dest('dist/'));
+});
+
+
 // Default task
-gulp.task('default', gulp.series('browser-sync', function () {
-    gulp.start('sass:watch');
-}));
+gulp.task('default', gulp.parallel('browser-sync', 'sass:watch'));
+
+// Build task
+gulp.task('build', gulp.series('clean', 'copyfonts', 'imagemin', 'usemin'));
