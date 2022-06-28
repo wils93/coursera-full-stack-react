ab2257002c5ef6456d990fadeda14fe51239918a
Andrew Wilson <andero.wil@gmail.com>
2022-06-28 19:01:48 +0200

    Add React components part 1

--- 02. Front-End Web Development with React/01. Weeks/React/confusion/src/App.js
+++ 02. Front-End Web Development with React/01. Weeks/React/confusion/src/App.js
@@ -1,19 +1,21 @@
 import { Component } from 'react';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/Menucomponent';
 
 class App extends Component {
     render() {
-      return (
-        <div className="App">
-          <Navbar dark color="primary">
-            <div className="container">
-              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+        return (
+            <div className="App">
+                <Navbar dark color="primary">
+                    <div className="container">
+                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                    </div>
+                </Navbar>
+                <Menu />
             </div>
-          </Navbar>
-        </div>
-      );
+        );
     }
-  }
+}
 
 export default App;
