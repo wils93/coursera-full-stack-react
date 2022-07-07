1507707af5d44e02a88152522cd9afea144f5ab9
Andrew Wilson <andero.wil@gmail.com>
2022-07-07 22:19:32 +0200

    Add React Route

--- 02. Front-End Web Development with React/01. Weeks/React/confusion/src/components/MainComponent.js
+++ 02. Front-End Web Development with React/01. Weeks/React/confusion/src/components/MainComponent.js
@@ -1,9 +1,11 @@
 import { Component } from 'react';
+import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
@@ -15,16 +17,22 @@ class Main extends Component {
         };
     }
 
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId });
-    }
-
     render() {
+
+        const HomePage = () => {
+            return (
+                <Home />
+            )
+        }
+
         return (
             <div className="App">
                 <Header />
-                <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Redirect to="/home" />
+                </Switch>
                 <Footer />
             </div>
         );
