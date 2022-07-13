15434ae57aef5f411e0f52c7eda649a5f94965fc
Andrew Wilson <andero.wil@gmail.com>
2022-07-13 12:54:23 +0200

    Add Single Page Application Part 2

--- 02. Front-End Web Development with React/01. Weeks/React/confusion/src/components/DishdetailComponent.js
+++ 02. Front-End Web Development with React/01. Weeks/React/confusion/src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDish({ dish }) {
     if (dish != null) {
@@ -23,6 +24,7 @@ function RenderDish({ dish }) {
 
 function RenderComments({ comments }) {
     if (comments != null) {
+        console.log(comments)
         return (
             <div className="col-12 col-md-5 m-1">
                 <h4>Comments</h4>
@@ -55,8 +57,18 @@ const DishDetail = (props) => {
         return (
             <div className="container">
                 <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
                     <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.dish.comments} />
+                    <RenderComments comments={props.comments} />
                 </div>
             </div>
         );
