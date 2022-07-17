8a5e088d6b8feee94fccd848885a7e308d6f0662
Andrew Wilson <andero.wil@gmail.com>
2022-07-17 18:34:46 +0200

    Add Redux Actions

--- 02. Front-End Web Development with React/01. Weeks/React/confusion/src/components/DishdetailComponent.js
+++ 02. Front-End Web Development with React/01. Weeks/React/confusion/src/components/DishdetailComponent.js
@@ -26,7 +26,7 @@ function RenderDish({ dish }) {
         );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, dishId }) {
     if (comments != null) {
         console.log(comments)
         return (
@@ -46,7 +46,7 @@ function RenderComments({ comments }) {
                         )
                     })}
                 </ul>
-                <CommentForm />
+                <CommentForm dishId={dishId} addComment={addComment} />
             </div>
         )
     }
@@ -80,8 +80,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log("Current State is: " + JSON.stringify(values));
-        alert("Current State is: " + JSON.stringify(values));
+        console.log(values)
+        this.props.addComment(this.props.dishId, values.rating, values.name, values.comment);
         this.toggleModal();
     }
 
@@ -155,7 +155,9 @@ const DishDetail = (props) => {
                 </div>
                 <div className="row">
                     <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments comments={props.comments}
+                        addComment={props.addComment}
+                        dishId={props.dish.id} />
                 </div>
             </div>
         );
