6aca7661c3843895d09813d11f9ce56d7191dbbb
Andrew Wilson <andero.wil@gmail.com>
2022-07-18 13:58:16 +0200

    Add Redux Thunk

--- 02. Front-End Web Development with React/01. Weeks/React/confusion/src/components/DishdetailComponent.js
+++ 02. Front-End Web Development with React/01. Weeks/React/confusion/src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import {
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 function RenderDish({ dish }) {
     if (dish != null) {
@@ -140,7 +141,25 @@ class CommentForm extends Component {
 }
 
 const DishDetail = (props) => {
-    if (props.dish != null)
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        )
+    }
+    else if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        )
+    }
+    else if (props.dish != null)
         return (
             <div className="container">
                 <div className="row">
