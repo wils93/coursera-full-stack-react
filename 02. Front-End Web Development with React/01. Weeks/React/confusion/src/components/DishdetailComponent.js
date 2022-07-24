a788832c6915a1e9ac1dc854722a26a6795d7be8
Andrew Wilson <andero.wil@gmail.com>
2022-07-24 13:47:03 +0200

    Fetch Post Comment

--- 02. Front-End Web Development with React/01. Weeks/React/confusion/src/components/DishdetailComponent.js
+++ 02. Front-End Web Development with React/01. Weeks/React/confusion/src/components/DishdetailComponent.js
@@ -28,7 +28,7 @@ function RenderDish({ dish }) {
         );
 }
 
-function RenderComments({ comments, addComment, dishId }) {
+function RenderComments({ comments, postComment, dishId }) {
     if (comments != null) {
         console.log(comments)
         return (
@@ -48,7 +48,7 @@ function RenderComments({ comments, addComment, dishId }) {
                         )
                     })}
                 </ul>
-                <CommentForm dishId={dishId} addComment={addComment} />
+                <CommentForm dishId={dishId} postComment={postComment} />
             </div>
         )
     }
@@ -83,7 +83,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         console.log(values)
-        this.props.addComment(this.props.dishId, values.rating, values.name, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.name, values.comment);
         this.toggleModal();
     }
 
@@ -176,7 +176,7 @@ const DishDetail = (props) => {
                 <div className="row">
                     <RenderDish dish={props.dish} />
                     <RenderComments comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         dishId={props.dish.id} />
                 </div>
             </div>
